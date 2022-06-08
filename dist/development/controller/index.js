cfadd98f2e4f865b9f2e0728186bf52b79583279
syifarahmat.id <syifa.mas@gmail.com>
2022-06-08 15:57:54 +0700

    git rm --cached dist -r

--- dist/development/controller/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", { value: true });exports.ping = ping;async function ping(req, res) {
-  return res.status(200).json({ status: 'success' });
-}
-//# sourceMappingURL=index.js.map
--- dist/development/util/variable.js
+++
@@ -1,2 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", { value: true });exports.clientsArray = void 0;let clientsArray = [];exports.clientsArray = clientsArray;
-//# sourceMappingURL=variable.js.map
